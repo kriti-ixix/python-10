e650007493d3849e497f71ec64f4902542ebf066
jasleen1901 <87968568+jasleen1901@users.noreply.github.com>
2021-08-17 12:10:46 +0530

    string done

---
+++ python/list.py
@@ -0,0 +1,19 @@
+#empty list
+l=list()
+print(l)
+#int elements
+l1=list([12,45,77,21])
+print(l1)
+l2=["hello", "hi"] #strings 
+print(l2)
+l3=["hello", 'h', 56,76.9]
+print(l3)
+l4=[1,2,3,4,5]
+print(l4)
+l5=list(range(1,100)) #range function
+print(l5[-4]) #accessing index
+print(l5[50:70]) #slicing
+l6=l2+l3  #join two list using +
+print(l6)
+l7=l4*3  #repeating elements using *
+print(l7)
--- python/string.py
+++ python/string.py
@@ -1,4 +1,4 @@
-s=str("HEllO")
+s=str("HELLO")
 s1="hello"
 print(s)
 print(s1)
@@ -14,3 +14,22 @@ index=1
 while index<3:
     print(s[index])
     index=index+1
+str1="i love python"
+print(str1[7:13])
+print(str1[::-1])
+s2="python "
+s3="programming"
+print(s2+s3)
+print(4*s2)
+print("u" in str1)
+print(len(str1))
+a=s1==s
+print(a)
+b=s1.upper()==s
+print(b)
+print(str1.split())
+print(str1[::-1])
+x=str1.split()
+print(x)
+x[0]="u"
+print(x)
