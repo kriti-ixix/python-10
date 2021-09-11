f93d58df2d08bc8ae907a66899cb2bb9207605cb
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-11 12:52:31 +0530

    api

---
+++ machine learning/diabetes-api/app.py
@@ -0,0 +1,30 @@
+#Importing the libraries
+import pickle
+from flask import Flask, render_template, request
+
+#Global Variables
+app = Flask(__name__)
+loadedModel = pickle.load(open('KNN Model.pkl', 'rb'))
+
+#Routes
+@app.route('/')
+def home():
+    return render_template('form.html')
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    glucose = request.form['glucose']
+    bmi = request.form['bmi']
+    age = request.form['age']
+
+    prediction = loadedModel.predict([[glucose, bmi, age]])
+
+    if prediction[0] == 0:
+        prediction = "Not Diabetic"
+    else:
+        prediction = "Diabetic"
+
+    return render_template('form.html', api_output=prediction)
+
+#Main function
+app.run(debug=True)
